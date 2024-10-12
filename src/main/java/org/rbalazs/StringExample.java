9ce6b0133ff0ec93c5878eb2a5b7cafd748085ef
Rodrigo Balazs <rodrigobalazs@gmail.com>
2024-10-12 20:15:18 -0300

    adds some javadocs and validations, updates pom.xml

--- src/main/java/org/rbalazs/StringExample.java
+++ src/main/java/org/rbalazs/StringExample.java
@@ -1,13 +1,23 @@
 package org.rbalazs;
 
+import org.apache.commons.lang3.Validate;
+
+/**
+ * Some examples of DSA(Data Structure and Algorithms),etc over Strings.
+ *
+ * @author Rodrigo Balazs
+ */
 public class StringExample {
 
+    /**
+     * Checks whether the string given as parameter it´s a palindrome or not.
+     *
+     * @param stringToCheck the string to check
+     * @return true if it´s a palindrome, false otherwise.
+     */
     public boolean isPalindrome(final String stringToCheck) {
-        if (stringToCheck == null) {
-            return false;
-        }
+        Validate.notNull(stringToCheck, "stringToCheck cannot be null");
         String stringReversed = new StringBuilder(stringToCheck).reverse().toString();
         return stringToCheck.equals(stringReversed);
     }
 }
-
